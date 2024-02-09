93de57474b449c30d75c18918b204bcbf70791a0
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-02-10 01:32:36 +0530

    Time: 59 ms, Memory: 79 MB - LeetHub

--- design-hashset/design-hashset.py
+++ design-hashset/design-hashset.py
@@ -1,35 +1,24 @@
-class MyHashSet(object):
-
-    def __init__(self):
-        self.data = [False] * 1000001
-
-    def add(self, key):
-        self.data[key] = True
-        """
-        :type key: int
-        :rtype: None
-        """
-        
-
-    def remove(self, key):
-        self.data[key] = False
-        """
-        :type key: int
-        :rtype: None
-        """
-        
-
-    def contains(self, key):
-        return self.data[key]
-        """
-        :type key: int
-        :rtype: bool
-        """
-        
-
-
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+class MyHashSet {
+public:
+    bool data[1000001]; // Array to simulate hash set
+    
+    // Constructor to initialize the hash set with default values (false)
+    MyHashSet() {
+        memset(data, false, sizeof data); // Initializing all elements with false using memset
+    }
+    
+    // Function to add a key to the hash set
+    void add(int key) {
+        data[key] = true; // Setting the value at the index corresponding to the key to true
+    }
+    
+    // Function to remove a key from the hash set
+    void remove(int key) {
+        data[key] = false; // Setting the value at the index corresponding to the key to false
+    }
+    
+    // Function to check if a key exists in the hash set
+    bool contains(int key) {
+        return data[key]; // Returning the value at the index corresponding to the key
+    }
+};
