5ca84df008fa48bd9896b75fea6cc5a6f34d8d61
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-02-10 02:26:06 +0530

    Time: 3 ms, Memory: 7.2 MB - LeetHub

--- happy-number/happy-number.cpp
+++ happy-number/happy-number.cpp
@@ -1,23 +1,26 @@
-#include <unordered_set>
+#include <cmath>
 
 class Solution {
 public:
+    // Function to calculate the sum of squares of digits of a number
+    int next(int x) {
+        int y = 0;
+        for (; x; x /= 10) {
+            y += pow(x % 10, 2);
+        }
+        return y;
+    }
+
     // Function to check if a number is a happy number
     bool isHappy(int n) {
-        unordered_set<int> vis; // Set to keep track of visited numbers
+        int slow = n, fast = next(n); // Initialize slow and fast pointers
 
-        // Continue the loop until n becomes 1 (happy number) or we encounter a cycle
-        while (n != 1 && !vis.count(n)) {
-            vis.insert(n); // Insert the current number into the set
-            int x = 0; // Initialize a variable to store the sum of squares of digits
-
-            // Calculate the sum of squares of digits of the current number
-            for (; n; n /= 10) {
-                x += (n % 10) * (n % 10);
-            }
-            n = x; // Update n with the sum of squares of digits
+        // Continue the loop until slow and fast pointers meet or slow becomes 1 (indicating a happy number)
+        while (slow != fast) {
+            slow = next(slow); // Move slow pointer by one step
+            fast = next(next(fast)); // Move fast pointer by two steps
         }
 
-        return n == 1; // If n becomes 1, it's a happy number; otherwise, it's not
+        return slow == 1; // If slow becomes 1, it's a happy number; otherwise, it's not
     }
 };
