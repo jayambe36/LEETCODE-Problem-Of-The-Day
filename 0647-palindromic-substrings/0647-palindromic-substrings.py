559f44a0ad95c8fa4dff3afa68d3bfd0eace8f53
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-02-10 21:44:13 +0530

    Time: 94 ms (49.46%), Space: 16.4 MB (93.37%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,6 +1,6 @@
-#Time complexity: $$O(n^2)$$. Space complexity: $$O(n)$$.
+#Time complexity: $$O(n^2)$$. Space complexity: $$O(1)$$.
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        return (f:=lambda l,r:0<=l<=r<len(s) and s[l]==s[r] and 1+f(l-1,r+1)) and \
-            sum(f(i,i)+f(i-1,i) for i in range(len(s)))
+        return (f:=lambda l,r:sum(takewhile(int,(s[l-i]==s[r+i] for i in range(min(l+1,len(s)-r)))))) and \
+            sum(f(i,i)+f(i-1,i) for i in range(1,len(s)))+1
