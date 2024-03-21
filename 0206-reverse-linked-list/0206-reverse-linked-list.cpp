acd6449642b2b932b585aaaa5af21035a7df7d3a
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-03-21 13:02:07 +0530

    Time: 2 ms (84.47%), Space: 11.9 MB (6.68%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if( !head || !head->next)
+        {
+            return head;
+        }
+        auto ans= reverseList(head->next);
+        
+        head->next->next=head;
+        head->next= NULL;
+        
+        return ans;
+    }
+};
