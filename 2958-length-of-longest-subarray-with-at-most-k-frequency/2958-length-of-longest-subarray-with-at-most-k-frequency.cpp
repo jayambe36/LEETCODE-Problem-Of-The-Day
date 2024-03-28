da55704e067b342f1742406f400d821402703795
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-03-28 09:54:05 +0530

    Time: 212 ms (58.19%), Space: 148.5 MB (57.21%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,16 +1,18 @@
-class Solution:
-    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
-        freq = {}
-        leng = len(nums)
-        res = 0
-        j = 0
-        for i in range(0, leng):
-            freq[nums[i]] = freq.get(nums[i], 0) + 1
-            while freq[nums[i]] > k:
-                freq[nums[j]] -= 1
-                j += 1
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        int j = 0;
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]]++;
 
-            res = max(res, i - j + 1)
-
-        return res
-
+            while (map[nums[i]] > k) {
+                map[nums[j]]--;
+                j++;
+            }
+            res = max(res, i - j + 1);
+        }
+        return res;
+    }
+};
