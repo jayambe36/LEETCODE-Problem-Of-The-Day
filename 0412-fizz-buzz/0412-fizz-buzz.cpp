db2958087042cac72e73b0889d6be9afa726de81
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-04-12 02:56:04 +0530

    Time: 5 ms (33.66%), Space: 10 MB (76.41%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> result;
-        for (int d = 1; d <= n; ++d) {
-            string str;
-            if (d % 3 == 0)
-                str += "Fizz";
-            if (d % 5 == 0)
-                str += "Buzz";
-            if (str.empty())
-                str = to_string(d);
-            result.push_back(str);
-        }
-        return result;
+        vector<string> fizzBuzz;
+        string temp;
+            for(int i=1; i <=n; i++){
+                string temp = "";
+                if( i % 3 == 0 || i % 5 == 0){
+                    if( i % 3 == 0 )
+                        temp = "Fizz";
+                    if ( i % 5 == 0)
+                        temp = temp + "Buzz";
+                }
+                else
+                    temp = to_string(i);
+                fizzBuzz.push_back(temp);
+            }
+        return fizzBuzz;
     }
 };
