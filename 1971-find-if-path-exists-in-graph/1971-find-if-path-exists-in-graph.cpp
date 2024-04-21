8a79fafbbb5d323707a453c27e1a3b49e5f0444c
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-04-21 11:41:42 +0530

    Time: 276 ms (92.71%), Space: 123.7 MB (90.20%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,30 +1,46 @@
-class Solution {
+class DSU {
+    vector<int> parent, rank;
 public:
+    DSU(int n){
+        parent.resize(n);
+        rank.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+    }
     
-    bool dfs(vector<vector<int>>& graph, vector<int>& visited, int current, int end) {
-        if(current == end)
+    int find(int x){
+        if(x !=parent[x])
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    
+    bool Union(int x, int y){
+        int xp = find(x);
+        int yp = find(y);
+        
+        if(xp == yp)
             return true;
-        if(visited[current])
+        else {
+            if(rank[xp] > rank[yp])
+                parent[yp] = xp;
+            else if (rank[xp] < rank[yp])
+                parent[xp] = yp;
+            else {
+                parent[yp] = xp;
+                rank[xp]++;
+            }
             return false;
-        
-        visited[current] = 1;
-        
-        for(int i=0; i<graph[current].size(); i++){
-            if(dfs(graph, visited, graph[current][i], end))
-                return true;
         }
-        
-        return false;
     }
     
-    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
-        vector<vector<int>> graph(n);
-        for(int i=0; i<edges.size(); i++) {
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
-        }
-        vector<int> visited(n, 0);
-        return dfs(graph, visited, start, end);
-    }
 };
 
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
+        DSU uf(n);
+        for(int i=0; i<edges.size(); i++) {
+            uf.Union(edges[i][0], edges[i][1]);
+        }
+        return uf.Union(start, end);
+    }
+};
