a3e5da0c385d2415c05da87d5d3d638dfb74c172
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-05-06 16:54:31 +0530

    Time: 247 ms (93.06%), Space: 164.4 MB (72.58%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if(head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode* nextN = removeNodes(head->next);
+        if(nextN->val > head->val) {
+            return nextN;
+        }
+        head->next = nextN;
+        return head;
+    }
+    
+};
