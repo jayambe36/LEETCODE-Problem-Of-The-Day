a2123a3ec0bf1b4fda19241a7ea6d084137321ca
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-07-07 12:28:33 +0530

    Time: 2 ms (53.84%), Space: 7.1 MB (30.43%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+       
+        int numWaterBottles(int numBottles, int numExchange) {
+        int res = numBottles;
+        while(numBottles >= numExchange)
+        {
+            int remainingBottles = numBottles%numExchange;
+            numBottles /= numExchange;
+            res += numBottles;
+            numBottles += remainingBottles;
+        }
+        return res;
+    }
+
+};
