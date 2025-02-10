7e499c7c30acb0fbf47ed0c649be2c13f7b50845
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2025-02-11 01:24:25 +0530

    Time: 0 ms (100%), Space: 17.7 MB (71.78%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack = []
+        for ch in s:
+            if ch.isdigit() and stack:
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        return ''.join(stack)
